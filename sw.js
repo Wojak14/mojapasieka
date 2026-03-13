40b2dfa34aec10c3b8b2fd54f4266c60d814d183
Wojak14 <andrzejbrodziuk@wp.pl>
2026-03-13 18:49:39 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,37 +4,28 @@ const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./bee_icon_192x192.png",
-  "./bee_icon_512x512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Instalacja
+// Instalacja SW i cache'owanie zasobów
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Aktywacja + czyszczenie starego cache
+// Aktywacja SW
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
-// Fetch
+// Obsługa fetch – najpierw cache, potem sieć, fallback do index.html
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() =>
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() =>
         caches.match("./index.html")
       );
     })
